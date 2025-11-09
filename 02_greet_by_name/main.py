c64715fc7e752e03a220c7c12f376afbd5354ff6
YuScripX <yusuf.tiftik@outlook.com>
2025-11-09 15:16:11 +0100

    add program to greet user by name

---
+++ 02_greet_by_name/main.py
@@ -0,0 +1,9 @@
+# 2️⃣ Greet by name
+# Vraag de gebruiker om zijn naam.
+# Print daarna "Hello, <naam>!"
+# Voorbeeld:
+# Input: Yusuf
+# Output: Hello, Yusuf!
+
+name = input("What is your name: ")
+print(f"Hello, {name}")
