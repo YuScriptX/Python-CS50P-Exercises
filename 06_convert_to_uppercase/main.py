d48d48c6d1196237698c736cd2698e8580b6abcb
YuScripX <yusuf.tiftik@outlook.com>
2025-11-10 10:12:11 +0100

    add function to convert and print name in uppercase

---
+++ 06_convert_to_uppercase/main.py
@@ -0,0 +1,12 @@
+# 6️⃣ Convert to uppercase
+# Vraag een naam aan de gebruiker.
+# Schrijf een functie shout(name) die de naam in hoofdletters print.
+# Voorbeeld:
+# Input: Yusuf
+# Output: YUSUF
+
+def shout(name):
+    print(name.upper())
+
+user = input("Enter your name: ")
+shout(user)
