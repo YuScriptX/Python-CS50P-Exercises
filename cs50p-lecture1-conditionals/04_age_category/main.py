f36d6f603bef0f2bd5211024db9682bca2a0f3ab
YuScripX <yusuf.tiftik@outlook.com>
2025-11-10 10:36:58 +0100

    add age category classification using if-elif-else conditions

---
+++ cs50p-lecture1-conditionals/04_age_category/main.py
@@ -0,0 +1,18 @@
+# 4️⃣ Age category
+# Vraag de leeftijd van de gebruiker.
+# Gebruik if/elif/else:
+# <13 → Child
+# 13–19 → Teenager
+# 20–64 → Adult
+# >=65 → Senior
+
+# age = int(input("Enter your age: "))
+
+# if age < 13:
+#     print("Child")
+# elif age <= 19:          
+#     print("Teenager")
+# elif age <= 64:          
+#     print("Adult")
+# else:                    
+#     print("Senior")
