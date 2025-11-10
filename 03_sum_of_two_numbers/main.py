50ca117262c7fb0a78a5993263fbdf64d0940d9f
YuScripX <yusuf.tiftik@outlook.com>
2025-11-10 10:04:21 +0100

    add program to calculate sum of two numbers

---
+++ 03_sum_of_two_numbers/main.py
@@ -0,0 +1,9 @@
+# 3️⃣ Sum of two numbers
+# Vraag twee getallen aan de gebruiker.
+# Bereken de som en print het resultaat.
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+total = num1 + num2
+
+print(total)
