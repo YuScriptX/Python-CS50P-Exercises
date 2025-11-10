3e6f06fded03b15b7db0eab291a5eb0599b77f9a
YuScripX <yusuf.tiftik@outlook.com>
2025-11-10 10:32:17 +0100

    add program to check if number is positive, negative, or zero

---
+++ cs50p-lecture1-conditionals/02_positive_negative_zero/main.py
@@ -0,0 +1,15 @@
+# 2️⃣ Positive, Negative, or Zero
+# Vraag een getal aan de gebruiker.
+# Gebruik if, elif, else:
+# > 0 → Positief
+# < 0 → Negatief
+# == 0 → Nul
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positief")
+elif number < 0:
+    print("Negatief")
+else:
+    print("Zero")
