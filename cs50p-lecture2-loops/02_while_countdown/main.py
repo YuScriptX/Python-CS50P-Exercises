21225cf930a3686f6e01c1c29ae33f1adfe1ac32
YuScripX <yusuf.tiftik@outlook.com>
2025-11-11 12:20:19 +0100

    feat: add while loop countdown exercise with blast off message

---
+++ cs50p-lecture2-loops/02_while_countdown/main.py
@@ -0,0 +1,18 @@
+# 2️⃣ while – Countdown
+# Gebruik een while-loop om af te tellen van 5 naar 1
+# en daarna "Blast off!" te printen.
+# Verwachte output:
+# 5
+# 4
+# 3
+# 2
+# 1
+# Blast off!
+
+
+i = 5
+
+while i > 0:
+    print(i)
+    i -= 1
+print("Blast off!")
