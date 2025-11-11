ee6f04b36dae6e0d6b35baa73b324820f5bbd31f
YuScripX <yusuf.tiftik@outlook.com>
2025-11-11 12:22:23 +0100

    feat: add for loop exercise to calculate sum from 1 to 100

---
+++ cs50p-lecture2-loops/03_sum_1_to_100/main.py
@@ -0,0 +1,12 @@
+# 3️⃣ Som van 1 t.e.m. 100
+# Gebruik een for-loop om de som te berekenen van 1 t.e.m. 100.
+# Print het resultaat.
+# Verwachte output:
+# 5050
+
+total = 0
+
+for i in range(1, 101):
+    total += i   
+
+print(total)
