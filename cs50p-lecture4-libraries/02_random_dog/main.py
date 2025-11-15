7fd8cb25423ef4128aea1e0c5afe69750cd04b98
YuScripX <yusuf.tiftik@outlook.com>
2025-11-15 16:23:08 +0100

    feat: add random dog image URL fetcher using requests

--- cs50p-lecture4-libraries/02_random_dog/main.py
+++ cs50p-lecture4-libraries/02_random_dog/main.py
@@ -2,3 +2,9 @@
 # Gebruik requests om een random dog image URL op te halen via:
 # https://dog.ceo/api/breeds/image/random
 # Print de image URL.
+
+import requests
+
+response = requests.get("https://dog.ceo/api/breeds/image/random")
+data = response.json()
+print(data["message"])
